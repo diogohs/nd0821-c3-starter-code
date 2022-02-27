af00a5d3fbd0aa463ca60d671fa01bc0104bd28d
diogohs <diogohshiraishi@gmail.com>
2022-02-27 11:48:46 -0300

    Add evaluate slices script

---
+++ starter/starter/evaluate_slices.py
@@ -0,0 +1,69 @@
+import pickle
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+from ml.data import process_data
+from ml.model import compute_model_metrics, inference
+
+
+def compute_metrics_on_test_set():
+    """Compute metrics on test set"""
+    # Load test data
+    data = pd.read_csv(r"../data/census_clean.csv", index_col=0)
+
+    # Train test split
+    _, test = train_test_split(data, test_size=0.20, random_state=42)
+
+    # Load model, encoder and label binarizer
+    with open("../model/model.pkl", "rb") as f:
+        model = pickle.load(f)
+
+    with open("../model/encoder.pkl", "rb") as f:
+        encoder = pickle.load(f)
+
+    with open("../model/label_binarizer.pkl", "rb") as f:
+        lb = pickle.load(f)
+
+    # Categorical features
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+
+    with open("slice_output.txt", "w") as file:
+        for col_value in test["education"].unique():
+            file.write(f"Slice - column value: {col_value}\n")
+
+            # Process the test data with the process_data function.
+            X_test, y_test, encoder, lb = process_data(
+                test.loc[test["education"] == col_value],
+                categorical_features=cat_features,
+                label="salary",
+                training=False,
+                encoder=encoder,
+                lb=lb,
+            )
+
+            # Get predictions
+            preds = inference(model, X_test)
+
+            # Calculate metrics
+            precision, recall, fbeta = compute_model_metrics(y_test, preds)
+
+            # Write slice metrics to file
+            file.write(f"Precision: {precision:.4f}\n")
+            file.write(f"Recall:    {recall:.4f}\n")
+            file.write(f"FBeta:     {fbeta:.4f}\n")
+            file.write(80 * "*")
+            file.write("\n")
+
+
+if __name__ == "__main__":
+    compute_metrics_on_test_set()
--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -3,13 +3,14 @@
 from sklearn.model_selection import train_test_split
 
 # Add the necessary imports for the starter code.
+import pickle
 import pandas as pd
 
 from ml.data import process_data
-from ml.model import train_model
+from ml.model import train_model, inference, compute_model_metrics
 
 # Add code to load in the data.
-data = pd.read_csv(r"starter/data/census_clean.csv")
+data = pd.read_csv(r"../data/census_clean.csv", index_col=0)
 
 # Optional enhancement, use K-fold cross validation
 # instead of a train-test split.
@@ -26,13 +27,29 @@ cat_features = [
     "native-country",
 ]
 
-# Proces the test data with the process_data function.
+# Process the train data with the process_data function.
 X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
 
 # Train and save a model.
 model = train_model(X_train, y_train)
-pd.to_pickle(model, r"../model/model.pkl")
-pd.to_pickle(lb, r"../model/label_binarizer.pkl")
-pd.to_pickle(encoder, r"../model/encoder.pkl")
+
+# Save artifacts
+pickle.dump(model, open("../model/model.pkl", "wb"))
+pickle.dump(encoder, open("../model/encoder.pkl", "wb"))
+pickle.dump(lb, open("../model/label_binarizer.pkl", "wb"))
+
+# Process the test data with the process_data function.
+X_test, y_test, encoder, lb = process_data(
+    test, categorical_features=cat_features, label="salary", training=False,
+    encoder=encoder, lb=lb
+)
+
+# Inference
+preds = inference(model, X_test)
+precision, recall, fbeta = compute_model_metrics(y_test, preds)
+
+print(f'Precision: {precision}')
+print(f'Recall: {recall}')
+print(f'FBeta: {fbeta}')
